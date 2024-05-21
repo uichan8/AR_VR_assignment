2582a2aca9a44b66a7a4e6c8653c8c7d7f7f02bc
Your Name <you@example.com>
2024-05-21 16:02:56 +0900

    temp

--- hw3/camera_pose.py
+++ hw3/camera_pose.py
@@ -19,8 +19,26 @@ def GetCameraPoseFromE(E):
     C_set : ndarray of shape (4, 3)
         The set of four camera centers
     """
+    #SVD
+    U, S, V = np.linalg.svd(E)
+    W = np.array([[0, -1, 0], [1, 0, 0], [0, 0, 1]])
+    R1 = U@W@V.T
+    R2 = U@W.T@V.T
+
+    C1 = U[:, 2]
+    C2 = -U[:, 2]
     
-    # TODO Your code goes here
+    R_set = np.zeros((4, 3, 3))
+    C_set = np.zeros((4, 3))
+    R_set[0] = R1
+    R_set[1] = R1
+    R_set[2] = R2
+    R_set[3] = R2
+
+    C_set[0] = C1
+    C_set[1] = C2
+    C_set[2] = C1
+    C_set[3] = C2
 
     return R_set, C_set
 
@@ -48,7 +66,20 @@ def Triangulation(P1, P2, track1, track2):
     """
     
     # TODO Your code goes here
+    val_track1 = np.bitwise_not(track1 == -1)
+    val_track1 = val_track1[:,0] + val_track1[:,1]
+    val_track2 = np.bitwise_not(track2 == -1)
+    val_track2 = val_track2[:,0] + val_track2[:,1]
+    val_track = val_track1 * val_track2
+    val_track = np.where(val_track == 2)
+    track1_coor = track1[val_track]
+    track2_coor = track2[val_track]
+    track1_coor = np.hstack((track1_coor, np.ones((track1_coor.shape[0],1))))
+    track2_coor = np.hstack((track2_coor, np.ones((track2_coor.shape[0],1))))
 
+    
+
+    X = None
     return X
 
 
@@ -99,7 +130,44 @@ def EstimateCameraPose(track1, track2):
     X : ndarray of shape (F, 3)
         The set of reconstructed 3D points
     """
-    
-    # TODO Your code goes here
+    # compute essential matrix
+    E, _ = EstimateE_RANSAC(track1, track2)
 
-    return R, C, X
+    # Estimate four configurations of poses
+    R_set, C_set = GetCameraPoseFromE(E)
+
+    for R,C in zip(R_set,C_set):
+        # Triangulate points using for each configuration
+        P1 = np.eye(3,4)
+        P2 = np.zeros((3,4))
+        P2[:3,:3] = R
+        P2[:,3] = C
+        X = Triangulation(P1,P2,track1,track2)
+
+    
+
+
+    #return R, C, X
+
+if __name__ == '__main__':
+    from feature import BuildFeatureTrack
+    import os
+    import cv2
+
+    img_list = os.listdir("im")
+    img_stack = []
+    for p in img_list:
+        img = cv2.imread(os.path.join("im", p))
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        img_stack.append(img)
+    img_stack = np.array(img_stack)
+    K = np.asarray([
+        [350, 0, 480],
+        [0, 350, 270],
+        [0, 0, 1]
+    ])
+    track = BuildFeatureTrack(img_stack, K)
+    track1 = track[0,:,:]
+    track2 = track[1,:,:]
+    EstimateCameraPose(track1, track2)
+
--- hw3/feature.py
+++ hw3/feature.py
@@ -1,5 +1,7 @@
 import cv2
 import numpy as np
+from tqdm import tqdm
+np.random.seed(42)
 
 
 
@@ -27,11 +29,29 @@ def MatchSIFT(loc1, des1, loc2, des2):
     ind1 : ndarray of shape (m,)
         The indices of x1 in loc1
     """
+    dis_array = []
 
-    # TODO Your code goes here
+    for i in range(des1.shape[0]):
+        dist = np.linalg.norm(des2 - des1[i], axis=1)
+        dis_array.append(dist)
+    dis_array = np.array(dis_array)
+    
+    argmin_des1 = np.argmin(dis_array, axis=1)
+    argmin_dis2 = np.argmin(dis_array, axis=0)
 
+    x1 = []
+    x2 = []
+    ind1 = []
+    for i in range(des1.shape[0]):
+        if argmin_dis2[argmin_des1[i]] == i:
+            x1.append(loc1[i])
+            x2.append(loc2[argmin_des1[i]])
+            ind1.append(i)
+    x1 = np.array(x1)
+    x2 = np.array(x2)
+    ind1 = np.array(ind1)
+            
     return x1, x2, ind1
-
 
 
 def EstimateE(x1, x2):
@@ -51,14 +71,20 @@ def EstimateE(x1, x2):
     E : ndarray of shape (3, 3)
         The essential matrix
     """
-    
-    # TODO Your code goes here
 
+    A = np.zeros((x1.shape[0], 9))
+    for i in range(x1.shape[0]):
+        x1_, y1_ = x1[i]
+        x2_, y2_ = x2[i]
+        A[i] = np.array([x2_*x1_, x2_*y1_, x2_, y2_*x1_, y2_*y1_, y2_, x1_, y1_, 1])
+
+    U, S, V = np.linalg.svd(A)
+    E = V[-1].reshape(3, 3)
     return E
 
 
 
-def EstimateE_RANSAC(x1, x2, ransac_n_iter, ransac_thr):
+def EstimateE_RANSAC(x1, x2, ransac_n_iter = 5000, ransac_thr = 0.5):
     """
     Estimate the essential matrix robustly using RANSAC
 
@@ -80,9 +106,26 @@ def EstimateE_RANSAC(x1, x2, ransac_n_iter, ransac_thr):
     inlier : ndarray of shape (k,)
         The inlier indices
     """
-    
-    # TODO Your code goes here
+    max_inliers = []
+    best_E = None
+    for _ in range(ransac_n_iter):
+        idx1 = np.random.choice(x1.shape[0], 8, replace=False)
+        idx2 = np.random.choice(x2.shape[0], 8, replace=False)
+        temp_E = EstimateE(x1[idx1], x2[idx2])
 
+        x1_ = np.hstack((x1, np.ones((x1.shape[0], 1))))
+        x2_ = np.hstack((x2, np.ones((x2.shape[0], 1))))
+        distanse_mat = (x2_.T*(temp_E@x1_.T)).sum(axis = 0)
+        #distanse_mat = np.einsum('ij,ji->i', x2, E@x1.T)
+        temp_inlier = np.where(np.abs(distanse_mat) < ransac_thr)[0]
+
+        if len(temp_inlier) > len(max_inliers):
+            max_inliers = temp_inlier
+            best_E = temp_E
+        
+    E = best_E
+    inlier = max_inliers
+        
     return E, inlier
 
 
@@ -104,6 +147,74 @@ def BuildFeatureTrack(Im, K):
         The feature tensor, where F is the number of total features
     """
     
-    # TODO Your code goes here
+    SIFT_feature = []
+    for i in range(Im.shape[0]):
+        img = Im[i]
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        sift = cv2.xfeatures2d.SIFT_create()
+        kp, des = sift.detectAndCompute(gray, None)
+        coordinates = np.array([k.pt for k in kp])
+        SIFT_feature.append((coordinates, des))
+
+    T = max([i[0].shape[0] for i in SIFT_feature])
+    #intialize the track
+    track = np.ones([Im.shape[0],T,2]) * -1
+    track[0,:SIFT_feature[0][0].shape[0]] = SIFT_feature[0][0]
+    print("Start matching")
+    for i in tqdm(range(Im.shape[0])):
+        track_i = []
+        for j in range(i+1,Im.shape[0]):
+            if i == j:
+                continue
+            i_loc, i_des = SIFT_feature[i]
+            j_loc, j_des = SIFT_feature[j]
+            x1, x2, ind1 = MatchSIFT(i_loc, i_des, j_loc, j_des)
+
+            #Normalize coordinate by multiplying the inverse of the intrinsic matrix
+            x1 = np.hstack((x1, np.ones((x1.shape[0], 1))))
+            x1 = np.linalg.inv(K)@x1.T
+            x1 = x1.T[:,:2]
+
+            x2 = np.hstack((x2, np.ones((x2.shape[0], 1))))
+            x2 = np.linalg.inv(K)@x2.T
+            x2 = x2.T[:,:2]
+
+            E, inlier = EstimateE_RANSAC(x1, x2, 10000, 0.5)
+            track_i_j = np.ones_like(i_loc) * -1
+            track_i_j[ind1[inlier]] = i_loc[ind1[inlier]]
+            track_i.append(track_i_j)
+        if i == j:
+            continue
+        track_i = np.array(track_i)
+        valid_track_i = np.bitwise_not(track_i == -1)
+        valid_track_i = valid_track_i[:,:,0] + valid_track_i[:,:,1]
+        valid_track_i = np.where(valid_track_i)
+        track[valid_track_i[0]+i+1,valid_track_i[1]] = track_i[valid_track_i[0],valid_track_i[1]]
+    
+    valid_track = np.bitwise_not(track == -1)
+    valid_track = valid_track[:,:,0] + valid_track[:,:,1]
+    valid_track = valid_track.sum(axis = 0) > 1
+    valid_track = np.where(valid_track)[0]
+    track = track[:,valid_track]
 
     return track
+
+if __name__ == "__main__":
+    import os
+    img_list = os.listdir("im")
+    img_stack = []
+    for p in img_list:
+        img = cv2.imread(os.path.join("im", p))
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        img_stack.append(img)
+    img_stack = np.array(img_stack)
+    K = np.asarray([
+        [350, 0, 480],
+        [0, 350, 270],
+        [0, 0, 1]
+    ])
+    track = BuildFeatureTrack(img_stack, K)
+
+
+
+
